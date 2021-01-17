a652e9142391dd72b15269d04c85c2e9e4f99451
Attachai <kraymanmarionette@gmail.com>
2021-01-17 12:26:04 +0700

    test_toDoList

--- test_todolist/lib/Screen/MainScreen.dart
+++ test_todolist/lib/Screen/MainScreen.dart
@@ -1,23 +1,124 @@
 import 'package:flutter/material.dart';
 
-class MainSreen extends StatefulWidget {
-  final String title;
+class MyHomePage extends StatefulWidget {
+  final String name;
 
-  const MainSreen({Key key, this.title}) : super(key: key);
-  
+  const MyHomePage({Key key, this.name}) : super(key: key);
+
   @override
-  _MainSreenState createState() => _MainSreenState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MainSreenState extends State<MainSreen> {
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> _todoItems = [];
+
+  void _addTodoItem(String task) {
+    setState(() {
+      if (task.length > 0) {
+        setState(() => _todoItems.add(task));
+      }
+    });
+  }
+
+  Widget _buildTodoList() {
+    return new ListView.builder(
+      itemBuilder: (context, index) {
+        if (index < _todoItems.length) {
+          return _buildTodoItem(_todoItems[index], index);
+        }
+      },
+    );
+  }
+
+  Widget _buildTodoItem(String todoText, int index) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          todoText,
+          style: Theme.of(context).textTheme.headline2), 
+
+        onTap: () => _promptRemoveTodoItem(index)
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(
+          'Todo List',
+          style: Theme.of(context).textTheme.headline2,
+        ),
         centerTitle: true,
+        backgroundColor: Theme.of(context).accentColor,
       ),
-
+      body: _buildTodoList(),
+      floatingActionButton: FloatingActionButton(onPressed: _addNewList, tooltip: 'Add task', child: Icon(Icons.add)),
     );
+  }
+
+
+  void _removeTodoItem(int index) {
+    setState(() => _todoItems.removeAt(index));
+  }
+
+  void _addNewList() {
+    var str;
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+              title: Text('Add New List'),
+              content: TextField(
+                autofocus: true,
+                onChanged: (val) {
+                  str = val;
+                },
+                style: Theme.of(context).textTheme.headline6,
+                decoration: InputDecoration(
+                  hintText: 'Enter something to do...',
+                  hintStyle: Theme.of(context).textTheme.bodyText1,
+             
+                  contentPadding: const EdgeInsets.all(16.0)),
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text(
+                    'CANCEL',
+                    style: Theme.of(context).textTheme.headline6.merge(
+                      TextStyle(
+                        color: Color(0xFFe63946)
+                      )
+                    ),
+                    ), 
+                  onPressed: () => Navigator.of(context).pop()),
+                FlatButton(
+                    child: new Text(
+                      'SVAE',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    onPressed: () {
+                      _addTodoItem(str);
+                      Navigator.pop(context); //
+                    }),
+              ]);
+        });
+  }
+
+  void _promptRemoveTodoItem(int index) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(title: Text('Mark "${_todoItems[index]}" as done?'), actions: <Widget>[
+            FlatButton(child: new Text('CANCEL'), onPressed: () => Navigator.of(context).pop()),
+            FlatButton(
+                child: new Text('MARK AS DONE'),
+                onPressed: () {
+                  _removeTodoItem(index);
+                  Navigator.of(context).pop();
+                })
+          ]);
+        });
   }
 }
--- test_todolist/lib/main.dart
+++ test_todolist/lib/main.dart
@@ -1,35 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:test_todolist/Screen/MainScreen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'Screen/MainScreen.dart';
+
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         fontFamily: 'ProductSans',
         primaryColor: Colors.white,
         floatingActionButtonTheme: FloatingActionButtonThemeData(elevation: 0, foregroundColor: Colors.white),
-        accentColor: Colors.black87,
+        accentColor: Color(0xFF1d3557),
         textTheme: TextTheme(
-          headline5: TextStyle(fontSize: 22.0, color: Colors.black, height: 1.3),
+          headline5: TextStyle(fontSize: 22.0, color: Color(0xFFa8dadc), height: 1.3),
           headline4: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w700, color: Color(0xFFCCCCCC), height: 1.1),
           headline3: TextStyle(fontSize: 36.0, fontWeight: FontWeight.w700, color: Color(0xDD000000), height: 1.3),
           headline2: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w700, color: Color(0xFF859566), height: 1.3),
           headline1: TextStyle(fontSize: 80.0, fontWeight: FontWeight.w300, color: Color(0xFF456268), height: 1.4),
           subtitle1: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500, color: Color(0xFFCCCCCC), height: 1.3),
-          headline6: TextStyle(fontSize: 17.0, fontWeight: FontWeight.w700, color: Color(0xFFCCCCCC), height: 1.2),
+          headline6: TextStyle(fontSize: 17.0, fontWeight: FontWeight.w700, color: Color(0xFF2a9d8f), height: 1.2),
           bodyText2: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400, color: Color(0xFFCCCCCC), height: 1.2),
           bodyText1: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w400, color: Color(0xFFCCCCCC), height: 1.2),
           caption: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w300, color: Color(0xFFCCCCCC), height: 1.2),
         ),
       ),
-      home: MainSreen(title: 'Todo List!!!'),
+      home: new MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
